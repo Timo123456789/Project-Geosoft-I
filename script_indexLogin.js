16e9c364be2f3e1c3cac321da6270e2b5006377b
Timo123456789 <48100977+Timo123456789@users.noreply.github.com>
2020-08-05 13:30:33 +0200

    First Version of Login Page (Textfields and Buttons)

---
+++ script_indexLogin.js
@@ -0,0 +1,14 @@
+function login(){
+console.log("Button funktioniert");
+
+}
+
+function createUserDoc(){
+    console.log(" Button createUserDoc funktioniert");
+    
+    }
+  
+    function createUserTrav(){
+        console.log(" Button createUserTrav funktioniert");
+        
+        }
