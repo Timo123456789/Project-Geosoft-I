220338f562bebdc527bb8c83dd99c25db593247e
Timo123456789 <48100977+Timo123456789@users.noreply.github.com>
2020-09-18 14:28:07 +0200

    add Documentation to Script Login

--- script_indexLogin.js
+++ script_indexLogin.js
@@ -1,20 +1,26 @@
 var IDDocs = 1;
 var IDTravs = 1;
 
-check_logged_User()
-login()
 
+login_LoginHTML()
+
+
+
+/**
+*@desc creates User as Doctor
+*@param  username_textfield = Value from Username_textfield
+*@param  password_textfield = Value from password_textfield
+*@param  IDDocs = ID for Doctors
+*
+*
+*
+*/
 
 function createUserDoc() {
 
   var username_textfield = document.getElementById("Username").value;
   var password_textfield = document.getElementById("Password").value;
-
-
   if (required(username_textfield) == true && required(password_textfield) == true) {
-
-
-
     var result = {
       Username: username_textfield,
       Password: password_textfield,
@@ -22,7 +28,6 @@ function createUserDoc() {
       userID: "D" + IDDocs,
     };
     IDDocs++;
-
     return new Promise(function (res, rej) {
       $.ajax({
         url: "/User",
@@ -37,7 +42,6 @@ function createUserDoc() {
       });
     })
   } else {
-
     window.alert("Please type something in Username and Password Textfield.");
   }
 
@@ -47,24 +51,27 @@ function createUserDoc() {
 
 }
 
+/**
+*@desc creates User as Travellers
+*@param  username_textfield = Value from Username_textfield
+*@param  password_textfield = Value from password_textfield
+*@param  IDTravs = ID for Travellers
+*
+*
+*
+*/
+
 function createUserTrav() {
-
-
-
   var username_textfield = document.getElementById("Username").value;
   var password_textfield = document.getElementById("Password").value;
-
   if (required(username_textfield) == true && required(password_textfield) == true) {
-
     var result = {
       Username: username_textfield,
       Password: password_textfield,
       role: "traveller",
-      RouteID: [],
       userID: "T" + IDTravs,
     };
     IDTravs++;
-
     return new Promise(function (res, rej) {
       $.ajax({
         url: "/User",
@@ -78,17 +85,21 @@ function createUserTrav() {
         error: function (err) { console.log(err) }
       });
     })
-
   }
-
   else {
-
     window.alert("Please type something in Username and Password Textfield.");
-
   }
 }
 
 
+
+/**
+*@desc function to check a textfield for strings
+*@param  inputtx = String of Textfield
+*@source https://www.w3resource.com/javascript/form/non-empty-field.php //minor changes 
+*
+*
+*/
 function required(inputtx) {
   if (inputtx == " ") {
     return false;
@@ -97,25 +108,28 @@ function required(inputtx) {
     return false;
   }
   return true;
-} //Source: https://www.w3resource.com/javascript/form/non-empty-field.php //minor changes 
+} 
 
 
 
 
 
-
+/**
+*@desc logged User in Server,check if User exists and  add Data from Logged User in Collection "Logged_User"
+*@param  username_textfield = Value from Username_textfield
+*@param  password_textfield = Value from password_textfield
+*@param  temp = Object with values from textfields for Get Request at Server at Search 
+*
+*
+*/
 
 function login() {
- // console.log("Button funktioniert");
-
   var username_textfield = document.getElementById("Username").value;
   var password_textfield = document.getElementById("Password").value;
-
   var temp = {
     Username: username_textfield,
     Password: password_textfield
   };
-
   return new Promise(function (res, rej) {
     $.ajax({
       url: "/User",
@@ -123,17 +137,11 @@ function login() {
       data: temp,
       success: function (result) {
         res(result);
-        console.log(res);
-        console.log(result);
-        if (result[0] != undefined) {
-          if (result[0].role == "traveller") {
-            console.log("traveller eingeloggt");
-            save_User_trav(result);
-           
-          } else if (result[0].role == "doctor") {
-            console.log("doctor eingeloggt");
-            save_User_Doc(result);
-        
+        if (result[0] != undefined) {  //Function find a User with password and username
+          if (result[0].role == "traveller") { //check if users role is Traveller
+            save_User_trav(result); //save user at Collection logged_user
+          } else if (result[0].role == "doctor") { //check if users role is doctor
+            save_User_Doc(result);//save user at Collection logged_user
           }
         }
         else {
@@ -143,19 +151,64 @@ function login() {
       error: function (err) { console.log(err) }
     });
   })
-
 }
 
+
+
+
+/**
+ * No Window Alerts Version
+*@desc logged User in Server,check if User exists and  add Data from Logged User in Collection "Logged_User"
+*@param  username_textfield = Value from Username_textfield
+*@param  password_textfield = Value from password_textfield
+*@param  temp = Object with values from textfields for Get Request at Server at Search 
+*
+*
+*/
+function login_LoginHTML() {
+  // console.log("Button funktioniert");
+ 
+   var username_textfield = document.getElementById("Username").value;
+   var password_textfield = document.getElementById("Password").value;
+ 
+   var temp = {
+     Username: username_textfield,
+     Password: password_textfield
+   };
+ 
+   return new Promise(function (res, rej) {
+     $.ajax({
+       url: "/User",
+       method: "GET",
+       data: temp,
+       success: function (result) {
+         res(result);
+         if (result[0] != undefined) {
+           if (result[0].role == "traveller") {
+             save_User_trav(result);      
+           } else if (result[0].role == "doctor") {
+             save_User_Doc(result);
+           }
+         }
+         else {
+          // window.alert("Please type something in Username and Password Textfield or check password and username!");
+         }
+       },
+       error: function (err) { console.log(err) }
+     });
+   })
+ 
+ }
+
+
+ /**
+*@desc saves User as Traveller at Collection "logged_User", connection to next HTML Site
+*@param  loggedUser = Object with Parameters
+*
+*
+*/
 function save_User_trav(loggedUser){
-
-  
-console.log("loggedUser");
-console.log(loggedUser);
 var result = loggedUser[0];
-console.log(result);
-
-    
-
     return new Promise(function (res, rej) {
       $.ajax({
         url: "/logged_User",
@@ -165,23 +218,22 @@ console.log(result);
           res(result);
           document.getElementById("Username").value = "";
           document.getElementById("Password").value = "";
-          window.location.href = 'http://localhost:3000/index_B';
+          window.location.href = 'http://localhost:3000/index_B';  //Connection to Index_b.html
         },
         error: function (err) { console.log(err) }
       });
     })
   } 
 
-  function save_User_Doc(loggedUser){
 
-  
-    console.log("loggedUser");
-    console.log(loggedUser);
+   /**
+*@desc saves User as Doctor at Collection "logged_User", connection to next HTML Site
+*@param  loggedUser = Object with Parameters
+*
+*
+*/
+  function save_User_Doc(loggedUser){
     var result = loggedUser[0];
-    console.log(result);
-    
-        
-    
         return new Promise(function (res, rej) {
           $.ajax({
             url: "/logged_User",
@@ -191,7 +243,7 @@ console.log(result);
               res(result);
               document.getElementById("Username").value = "";
               document.getElementById("Password").value = "";
-              window.location.href = 'http://localhost:3000/index_doc';
+              window.location.href = 'http://localhost:3000/index_doc'; //Connection to index_doc.html
             },
             error: function (err) { console.log(err) }
           });
@@ -199,6 +251,10 @@ console.log(result);
       } 
 
 
+
+/**
+*@desc delete all Values in Collection "logged_User"
+*/
  function delete_logged_User(){
 
   return new Promise(function (res, rej) {
@@ -217,7 +273,6 @@ console.log(result);
 
 
  
-
 
 
 
