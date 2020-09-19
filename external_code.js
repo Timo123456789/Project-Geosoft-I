c6a797f8735039ad4861a70b25699a05a11f63bc
JudithBre <61976163+JudithBre@users.noreply.github.com>
2020-09-20 00:44:22 +0200

    function openCity is sorted out

--- external_code.js
+++ external_code.js
@@ -21,3 +21,26 @@
 function druck() {
   window.print();
 }
+
+/** @description The Window.print() method starts the printing process
+ *               identical to the menu command for printing in the browser
+ *
+ */
+
+ /**
+ *@desc convert point in GJSON Feature
+ *@param  i = temporary variable
+ *@param x = Feature Collection of GeoJSON
+ *
+ */
+
+/* https://www.w3schools.com/w3css/w3css_tabulators.asp */
+
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(cityName).style.display = "block";
+}
